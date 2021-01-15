c171d37af95afadc379f2e624229a79a61e9d565
erenturkk1 <77416891+erenturkk1@users.noreply.github.com>
2021-01-15 12:13:16 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -4,7 +4,7 @@ exports.run = (client, message) => {
   const WestraEmbed = new WestraDiscord.MessageEmbed();
   WestraEmbed.setColor(0x36393F)
 //  WestraEmbed.setAuthor(`Crypto'yu davet ederek sunucunu yapılandır!`)
-  WestraEmbed.setDescription(`<a:twitchbit:793899916614828062> Carnoxis'yi davet etmek için [buraya](https://discord.com/oauth2/authorize?client_id=793883542782214174&scope=bot&permissions=8) tıkla! \n \n <a:twitchbit:793899916614828062> Carnoxis'in Destek Sunucusu için [buraya](https://discord.gg/dn7J8vDJ3V) tıkla`)
+  WestraEmbed.setDescription(`<a:twitchbit:793899916614828062> Carnoxis'yi davet etmek için [buraya](https://discord.com/oauth2/authorize?client_id=793883542782214174&scope=bot&permissions=8) tıkla! \n \n <a:twitchbit:793899916614828062> Carnoxis'in Destek Sunucusu için [buraya](https://discord.gg/dn7J8vDJ3V) tıkla \n \n <a:twitchbit:793899916614828062> Carnoxis'in Sitesi için [buraya](http://www.carnoxisbot.tk) tıkla`)
   message.channel.send(WestraEmbed)
 }
 exports.conf = {
